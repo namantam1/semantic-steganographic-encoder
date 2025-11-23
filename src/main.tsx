4fcb18fa75ee5330e1d5f3c1d766da0246797675
namantam1 <namantam1@gmail.com>
2025-11-24 04:07:19 +0530

    Fix 404

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 import Visualizer from './Visualizer';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <App />,
@@ -13,9 +13,7 @@ const router = createBrowserRouter([
     path: '/visualizer',
     element: <Visualizer />,
   },
-], {
-  basename: import.meta.env.BASE_URL,
-});
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
